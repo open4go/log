8fb89e8a08d8eedd69bf0e51e6bdff047b6bd049
imac <imac@192.168.1.6>
2023-07-23 12:03:51 +0800

    fixed pkg name

--- model/login/method.go
+++ model/login/method.go
@@ -1,12 +1,5 @@
 package login
 
-import (
-	rtime "github.com/r2day/base/time"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"time"
-)
-
 // ResourceName 返回资源名称
 func (m *Model) ResourceName() string {
 	return modelName
