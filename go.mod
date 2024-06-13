4b4265d851abe406487bbcb28e3da71ec2d04ae7
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-06-14 01:13:20 +0800

    add docker meta logs

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/open4go/log
 
-go 1.20
+go 1.21
+
+toolchain go1.22.3
 
 require (
 	github.com/open4go/model v0.0.4
@@ -10,17 +12,30 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/docker v26.1.4+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/r2day/base v1.6.7 // indirect
 	github.com/r2day/db v0.3.5 // indirect
 	github.com/redis/go-redis/v9 v9.0.3 // indirect
@@ -33,9 +48,13 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.52.0 // indirect
+	go.opentelemetry.io/otel v1.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- log.go
+++ log.go
@@ -1,14 +1,81 @@
 package log
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/docker/docker/client"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"golang.org/x/net/context"
 	"io"
+	"log"
 	"os"
 	"runtime"
 	"strconv"
 	"strings"
+	"time"
 )
+
+// LogEntry represents a single log entry with additional metadata
+type LogEntry struct {
+	File       string `json:"file"`
+	Func       string `json:"func"`
+	Level      string `json:"level"`
+	Msg        string `json:"msg"`
+	Port       string `json:"port"`
+	Prefix     string `json:"prefix"`
+	Server     string `json:"server"`
+	Time       string `json:"time"`
+	Image      string `json:"image"`
+	Container  string `json:"container"`
+	InstanceID string `json:"instance_id"`
+}
+
+// getDockerMetadata fetches the Docker container metadata
+func getDockerMetadata() (string, string, string, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return "", "", "", err
+	}
+
+	ctx := context.Background()
+	containerID := os.Getenv("HOSTNAME")
+	containerJSON, err := cli.ContainerInspect(ctx, containerID)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	return containerJSON.Config.Image, containerJSON.Name, containerID, nil
+}
+
+func demo() {
+	image, container, instanceID, err := getDockerMetadata()
+	if err != nil {
+		log.Fatalf("Failed to get Docker metadata: %v", err)
+	}
+
+	// Example log entry
+	entry := LogEntry{
+		File:       "init.go:53",
+		Func:       "loadRoutesAndStartServer",
+		Level:      "info",
+		Msg:        "Server is running successfully",
+		Port:       "8080",
+		Prefix:     "/v1/order",
+		Server:     "order-api",
+		Time:       time.Now().Format(time.RFC3339),
+		Image:      image,
+		Container:  container,
+		InstanceID: instanceID,
+	}
+
+	logData, err := json.Marshal(entry)
+	if err != nil {
+		log.Fatalf("Failed to marshal log entry: %v", err)
+	}
+
+	fmt.Println(string(logData))
+}
 
 var logger = logrus.New()
 
@@ -50,7 +117,17 @@ func Log() *logrus.Entry {
 	filename := file[strings.LastIndex(file, "/")+1:] + ":" + strconv.Itoa(line)
 	funcName := runtime.FuncForPC(pc).Name()
 	fn := funcName[strings.LastIndex(funcName, ".")+1:]
+
+	// 获取镜像元数据
+	image, container, instanceID, err := getDockerMetadata()
+	if err != nil {
+		log.Fatalf("Failed to get Docker metadata: %v", err)
+	}
+
 	return logger.WithField("file", filename).
 		WithField("func", fn).
-		WithField("server", serverName)
+		WithField("server", serverName).
+		WithField("image", image).
+		WithField("container", container).
+		WithField("instance", instanceID)
 }
